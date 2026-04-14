9a2979905962fd381b9c5ce0c56276d802ba06f8
256-katendechris-256 <katendechris5@gmail.com>
2026-04-14 21:49:51 +0300

    migrations

--- migrate_prod.py
+++ migrate_prod.py
@@ -7,27 +7,44 @@ BASE_DIR = Path(__file__).resolve().parent
 sys.path.insert(0, str(BASE_DIR))
 load_dotenv(BASE_DIR / '.env', override=True)
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.base"
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['SUPABASE_DB_NAME'],
-        'USER': os.environ['SUPABASE_DB_USER'],
-        'PASSWORD': os.environ['SUPABASE_DB_PASSWORD'],
-        'HOST': os.environ['SUPABASE_DB_HOST'],
-        'PORT': os.environ['SUPABASE_DB_PORT'],
-        'OPTIONS': {'sslmode': 'require'},
-    }
-}
-
 from django.conf import settings
-settings.DATABASES = DATABASES
+
+settings.configure(
+    BASE_DIR=BASE_DIR,
+    INSTALLED_APPS=[
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'rest_framework',
+        'corsheaders',
+        'notifications',
+        'apps.accounts',
+        'apps.books',
+        'apps.reading',
+        'apps.clubs',
+        'apps.discussions',
+        'apps.gamification',
+    ],
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['SUPABASE_DB_NAME'],
+            'USER': os.environ['SUPABASE_DB_USER'],
+            'PASSWORD': os.environ['SUPABASE_DB_PASSWORD'],
+            'HOST': os.environ['SUPABASE_DB_HOST'],
+            'PORT': os.environ['SUPABASE_DB_PORT'],
+            'OPTIONS': {'sslmode': 'require'},
+        }
+    },
+    AUTH_USER_MODEL='accounts.User',
+    DEFAULT_AUTO_FIELD='django.db.models.BigAutoField',
+)
 
 import django
-from django import db
-db.connections = db.ConnectionHandler(DATABASES)
 django.setup()
 
 from django.core.management import call_command
-call_command('migrate', verbosity=1)
+call_command('migrate', verbosity=2)
